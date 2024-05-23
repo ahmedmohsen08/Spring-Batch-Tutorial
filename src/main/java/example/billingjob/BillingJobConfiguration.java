b264860b43f49ca14ab1baa54f35ddb4ef9082ac
ahmedmohsen <ahmedmohsen0888@gmail.com>
2024-05-23 11:43:55 +0000

    finished tutorial

--- src/main/java/example/billingjob/BillingJobConfiguration.java
+++ src/main/java/example/billingjob/BillingJobConfiguration.java
@@ -48,7 +48,8 @@ public class BillingJobConfiguration {
 
 	@Bean
 	@StepScope
-	public FlatFileItemReader<BillingData> billingDataFileReader(@Value("#{jobParameters['input.file']}") String inputFile) {
+	public FlatFileItemReader<BillingData> billingDataFileReader(
+			@Value("#{jobParameters['input.file']}") String inputFile) {
 		return new FlatFileItemReaderBuilder<BillingData>()
 				.name("billingDataFileReader")
 				.resource(new FileSystemResource(inputFile))
@@ -76,9 +77,9 @@ public class BillingJobConfiguration {
 
 	@Bean
 	public Step step2(JobRepository jobRepository, JdbcTransactionManager transactionManager,
-					  ItemReader<BillingData> billingDataFileReader,
-					  ItemWriter<BillingData> billingDataTableWriter,
-					  BillingDataSkipListener skipListener) {
+			ItemReader<BillingData> billingDataFileReader,
+			ItemWriter<BillingData> billingDataTableWriter,
+			BillingDataSkipListener skipListener) {
 		return new StepBuilder("fileIngestion", jobRepository)
 				.<BillingData, BillingData>chunk(100, transactionManager)
 				.reader(billingDataFileReader)
@@ -92,7 +93,9 @@ public class BillingJobConfiguration {
 
 	@Bean
 	@StepScope
-	public JdbcCursorItemReader<BillingData> billingDataTableReader(DataSource dataSource, @Value("#{jobParameters['data.year']}") Integer year, @Value("#{jobParameters['data.month']}") Integer month) {
+	public JdbcCursorItemReader<BillingData> billingDataTableReader(DataSource dataSource,
+			@Value("#{jobParameters['data.year']}") Integer year,
+			@Value("#{jobParameters['data.month']}") Integer month) {
 		String sql = String.format("select * from BILLING_DATA where DATA_YEAR = %d and DATA_MONTH = %d", year, month);
 		return new JdbcCursorItemReaderBuilder<BillingData>()
 				.name("billingDataTableReader")
@@ -103,38 +106,43 @@ public class BillingJobConfiguration {
 	}
 
 	@Bean
-	public BillingDataProcessor billingDataProcessor() {
-		return new BillingDataProcessor();
+	public BillingDataProcessor billingDataProcessor(PricingService pricingService) {
+		return new BillingDataProcessor(pricingService);
 	}
 
 	@Bean
 	@StepScope
-	public FlatFileItemWriter<ReportingData> billingDataFileWriter(@Value("#{jobParameters['output.file']}") String outputFile) {
+	public FlatFileItemWriter<ReportingData> billingDataFileWriter(
+			@Value("#{jobParameters['output.file']}") String outputFile) {
 		return new FlatFileItemWriterBuilder<ReportingData>()
 				.resource(new FileSystemResource(outputFile))
 				.name("billingDataFileWriter")
 				.delimited()
-				.names("billingData.dataYear", "billingData.dataMonth", "billingData.accountId", "billingData.phoneNumber", "billingData.dataUsage", "billingData.callDuration", "billingData.smsCount", "billingTotal")
+				.names("billingData.dataYear", "billingData.dataMonth", "billingData.accountId",
+						"billingData.phoneNumber", "billingData.dataUsage", "billingData.callDuration",
+						"billingData.smsCount", "billingTotal")
 				.build();
 	}
 
 	@Bean
 	public Step step3(JobRepository jobRepository, JdbcTransactionManager transactionManager,
-							   ItemReader<BillingData> billingDataTableReader,
-							   ItemProcessor<BillingData, ReportingData> billingDataProcessor,
-							   ItemWriter<ReportingData> billingDataFileWriter) {
+			ItemReader<BillingData> billingDataTableReader,
+			ItemProcessor<BillingData, ReportingData> billingDataProcessor,
+			ItemWriter<ReportingData> billingDataFileWriter) {
 		return new StepBuilder("reportGeneration", jobRepository)
 				.<BillingData, ReportingData>chunk(100, transactionManager)
 				.reader(billingDataTableReader)
 				.processor(billingDataProcessor)
 				.writer(billingDataFileWriter)
+				.faultTolerant()
+				.retry(PricingException.class)
+				.retryLimit(100)
 				.build();
 	}
 
 	@Bean
 	public PricingService pricingService() {
-		return  new PricingService();
+		return new PricingService();
 	}
-
 
 }
